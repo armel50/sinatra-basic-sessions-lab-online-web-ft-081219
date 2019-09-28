da28f3a524ce2782b7d46002158629c438443d9b
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-28 10:05:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,9 +10,10 @@ class App < Sinatra::Base
   
   post '/checkout' do 
     
-    configure do 
-      enable :sessions
-    end
+   configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
    
     session[:item] = params[:item]
     @session = session
